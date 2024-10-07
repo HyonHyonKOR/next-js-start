31e6bfa17b7fb025be0e64b10628cf5f335cc315
HyonHyon <khm3626@gmail.com>
2024-10-07 21:34:00 +0900

    feat:Promise all

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,10 @@
-import { resolve } from "path";
+import Link from "next/link";
 
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   const response = await fetch(URL);
@@ -14,5 +14,14 @@ async function getMovies() {
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div> {JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {" "}
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
