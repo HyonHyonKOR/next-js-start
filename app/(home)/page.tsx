9d7d9c10aa80fcf992cc513afe200ba3f83cc47d
HyonHyon <khm3626@gmail.com>
2024-10-10 19:46:46 +0900

    feat:add css module

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Movie from "../../components/movie";
 
 export const metadata = {
   title: "Home",
@@ -17,10 +18,6 @@ export default async function HomePage() {
   return (
     <div>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <img src={movie.poster_path} alt={movie.title} />
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
       ))}
     </div>
   );
