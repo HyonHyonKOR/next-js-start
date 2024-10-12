544f1d417a2e325676389b497497311d0d7b7137
HyonHyon <khm3626@gmail.com>
2024-10-12 22:45:15 +0900

    feat:add video api

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,13 +1,24 @@
 import { URL } from "../app/(home)/page";
+import style from "../styles/movie-video.module.css";
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("something broke...");
-  //const response = await fetch(`${URL}/${id}/videos`);
-  //return response.json();
+  const response = await fetch(`${URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={style.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
